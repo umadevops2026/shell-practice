cdfb4c1077f0439d02173b613990ddc421ab8213
Pennabadi Umamaheswar Reddy <pennabadiumamaheswarreddy@Pennabadis-MacBook-Air.local>
2026-09-17 08:13:33 +0800

    some message

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+PERSON1=Uma
+PERSON2=Muni
+echo "$PERSON1 :: Hi $PERSON2, How are you?"
+echo "$PERSON2 :: Hello $PERSON1, I am fine, how are you?"
+echo "$PERSON1 :: What are you learning recently"
+echo "$PERSON2 :: I am learning Shell scripting $PERSON1"

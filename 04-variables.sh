ff6318217dc7d5b43cbab770589a488927c06022
Pennabadi Umamaheswar Reddy <pennabadiumamaheswarreddy@Pennabadis-MacBook-Air.local>
2026-09-18 23:16:07 +0800

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: welcome to alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on Zoom meeting"
+echo "$PERSON1:: So do we have a deal"
+echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
